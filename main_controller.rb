0edfa054a5808d1469438e1ff914fbc148f0842f
Mungo Dewar <mpjdewar@gmail.com>
2017-05-05 15:22:58 +0100

    Add @albums.all to index.erb

--- main_controller.rb
+++ main_controller.rb
@@ -1,10 +1,12 @@
 require('sinatra')
 require('sinatra/contrib/all')
+require_relative('models/album')
 
 get '/' do
   redirect to('/albums')
 end
 
 get '/albums' do
+  @albums = Album.all()
   erb(:index)
 end
