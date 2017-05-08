d052b4687510249ce762e3228bd8ac8dff5cb479
Mungo Dewar <mpjdewar@gmail.com>
2017-05-08 10:32:44 +0100

    Change session key name

--- controllers/album_controller.rb
+++ controllers/album_controller.rb
@@ -2,9 +2,11 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/helper')
 require_relative('../models/album')
+require_relative('../models/artist')
 
 get '/albums' do
   @albums = Album.all()
+  @artists = Artist.all()
   session.delete(:filter)
   erb :"albums/index" do
     erb :"filters/form"
@@ -23,12 +25,12 @@ end
 
 post '/albums/:id/add_stock' do
    Album.find(params['id']).add_stock.update
-  redirect "/filter?stock_level=#{session[:filter]}", 307 if session[:filter]
+  redirect "/filter?stock_level=#{session[:stock_level]}", 307 if session[:stock_level]
   redirect to ('/albums')
 end
 
 post '/albums/:id/remove_stock' do
    Album.find(params['id']).remove_stock.update
-  redirect "/filter?stock_level=#{session[:filter]}", 307 if session[:filter]
+  redirect "/filter?stock_level=#{session[:stock_level]}", 307 if session[:stock_level]
   redirect to ('/albums')
 end
--- controllers/filter_controller.rb
+++ controllers/filter_controller.rb
@@ -4,8 +4,9 @@ require_relative('../models/album')
 
 post '/filter' do
   stock_level = params['stock_level']
-  session[:filter] = stock_level
+  session[:stock_level] = stock_level
   @albums = Album.filter_stock_level(stock_level)
+  @artists = Artist.all()
   erb :"albums/index" do
     erb :"filters/form"
   end
