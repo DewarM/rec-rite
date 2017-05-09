cb0fb4c1ee7258bfb6ef562cc80446618f4eeb50
Mungo Dewar <mpjdewar@gmail.com>
2017-05-09 11:51:35 +0100

    Refactor each loop to map

--- models/search.rb
+++ models/search.rb
@@ -8,11 +8,13 @@ class Search
   end
 
   def search_albums()
-    results = []
-    FuzzyMatch.new(Album.all, :read => :title, :threshold => 0.2).find_all_with_score(@search_query).each do |record, a, b|
-      results << record
+    FuzzyMatch.new(
+      Album.all,
+      :read => :title,
+      :threshold => 0.2
+    ).find_all_with_score(@search_query).map do |record, score_d, score_l|
+      record
     end
-    return results
   end
 
 
